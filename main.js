5a4bc375bd439b08f848f2cd0bb1ad74b335fe84
thefrenchpaki <33563185+thefrenchpaki@users.noreply.github.com>
2018-03-21 21:15:30 -0400

    client

---
+++ main.js
@@ -0,0 +1,26 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    var template = Template.instance();
+    web3.eth.getBalance("0xbb4a8f07bEc96FA45687efDb6E07CDaD5Dd5824C",
+              function (err, res){
+                TemplateVar.set(template, "counter", res);
+              })
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
